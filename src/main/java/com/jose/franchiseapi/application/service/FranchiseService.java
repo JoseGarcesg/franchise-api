8552040dfa9b2f3aaa0d8d7828b756ac8c95d7ab
JoseGarces <jose.garcesg@gmail.com>
2026-03-07 22:22:18 -0500

    feat: add branh to franchise and list franchise

--- src/main/java/com/jose/franchiseapi/application/service/FranchiseService.java
+++ src/main/java/com/jose/franchiseapi/application/service/FranchiseService.java
@@ -1,9 +1,12 @@
 package com.jose.franchiseapi.application.service;
 
+import com.jose.franchiseapi.domain.model.Branch;
 import com.jose.franchiseapi.domain.model.Franchise;
+import com.jose.franchiseapi.domain.model.Product;
 import com.jose.franchiseapi.domain.repository.FranchiseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
@@ -13,6 +16,10 @@ import java.util.ArrayList;
 public class FranchiseService {
     private final FranchiseRepository repository;
 
+    public Flux<Franchise> getAllFranchises() {
+        return repository.findAll();
+    }
+
     public Mono<Franchise> createFranchise(String name) {
 
         Franchise franchise = new Franchise();
@@ -20,4 +27,18 @@ public class FranchiseService {
 
         return repository.save(franchise);
     }
+
+    public Mono<Franchise> addBranch(String franchiseId, Branch branch) {
+
+        return repository.findById(franchiseId)
+                .switchIfEmpty(Mono.error(new RuntimeException("Franchise not found")))
+                .flatMap(franchise -> {
+
+                    franchise.getBranches().add(branch);
+
+                    return repository.save(franchise);
+                });
+    }
+
+
 }
--- src/main/java/com/jose/franchiseapi/interfaces/controller/FranchiseController.java
+++ src/main/java/com/jose/franchiseapi/interfaces/controller/FranchiseController.java
@@ -1,12 +1,11 @@
 package com.jose.franchiseapi.interfaces.controller;
 
 import com.jose.franchiseapi.application.service.FranchiseService;
+import com.jose.franchiseapi.domain.model.Branch;
 import com.jose.franchiseapi.domain.model.Franchise;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -14,8 +13,20 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class FranchiseController {
     private final FranchiseService service;
+    @GetMapping
+    public Flux<Franchise> getAllFranchises() {
+        return service.getAllFranchises();
+    }
+
     @PostMapping
     public Mono<Franchise> createFranchise(@RequestBody Franchise franchise) {
         return service.createFranchise(franchise.getName());
+    }
+
+
+    @PostMapping("/{franchiseId}/branches")
+    public Mono<Franchise> addBranch(@PathVariable String franchiseId,
+                                     @RequestBody Branch branch) {
+        return service.addBranch(franchiseId, branch);
     }
 }
